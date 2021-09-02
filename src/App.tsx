d35be19b0a0c5d260a245e8ed4b9e7bb6f23056e
Giancarlo Cavalli <gc.giancarloo@gmail.com>
2021-09-02 07:02:47 -0300

    Adding Bootstrap and Sass

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import './App.css';
 
 function App() {
@@ -6,6 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
       </header>
+      <h1>HELLO DEV</h1>
     </div>
   );
 }
