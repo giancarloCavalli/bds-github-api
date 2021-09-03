b46d45cb7f76c081e394fffdde11a5dd564c2a6d
Giancarlo Cavalli <gc.giancarloo@gmail.com>
2021-09-02 21:57:37 -0300

    Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import './App.css';
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
       </header>
-      <h1>HELLO DEV</h1>
+      <Navbar />
     </div>
   );
 }
---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,11 @@
+import './styles.css';
+
+const Navbar = () => {
+  return (
+    <div className="page-container bg-primary">
+      <h5 className="title">Github API</h5>
+    </div>
+  )
+}
+
+export default Navbar;
