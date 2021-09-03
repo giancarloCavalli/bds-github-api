3a5310ad29d2d8e29e08c4c0b02ff0045f995d7f
Giancarlo Cavalli <gc.giancarloo@gmail.com>
2021-09-03 18:43:22 -0300

    Home Page styles

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,15 @@
+import './styles.css';
+
 const Home = () => {
   return (
     <div className="home-container">
       <div className="home-header">
         <h1>Desafio Github API</h1>
       </div>
-      <div>
-        <p>Bootcamp Spring React - DevSuperior</p>
+      <div className="home-content">
+        <h6>Bootcamp Spring React - DevSuperior</h6>
       </div>
-      <div>
+      <div className="home-bottom">
         <button className="btn-primary">Começar</button>
       </div>
     </div>
