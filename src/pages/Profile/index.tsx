e7a869218f95bcc2cb7dedb844f1f25994742bcb
Giancarlo Cavalli <gc.giancarloo@gmail.com>
2021-09-05 18:00:23 -0300

    Github API Request

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,15 +1,47 @@
+import axios from 'axios';
+import React, { useState } from 'react';
 import UserCard from '../../components/UserCard';
+import { User } from '../../types/User';
+import { GIT_USER_URL } from '../../util/requests';
 import './styles.css';
 
+type SearchData = {
+  username: string;
+}
+
 const Profile = () => {
+
+  const [user, setUser] = useState<User>();
+
+  const [searchData, setSearchData] = useState<SearchData>({
+    username: ''
+  });
+
+  const handleClick = () => {
+    axios.get(`${GIT_USER_URL}/${searchData?.username}`)
+    .then(response => {
+      setUser(response.data);
+      console.log(response.data);
+    })
+    .catch(error => {
+      console.log(error);
+    })
+  }
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setSearchData({ ...searchData, [name]: value});
+  };
+
   return (
     <div className="prof-search-container">
       <div className="prof-search-box">
         <h1>Encontre um perfil Github</h1>
         <div>
-          <input type="text" name="" id="" />
+          <input type="text" name="username" value={searchData.username} placeholder="Username" onChange={handleChange} />
         </div>
-        <button className="btn-primary">Encontrar</button>
+        <button className="btn-primary" onClick={handleClick}>Encontrar</button>
       </div>
     <UserCard />
     </div>
